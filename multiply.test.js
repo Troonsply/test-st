ea1639add6f3071b91345218754ce2464f499264
troonsply <troonsply@gmail.com>
2024-05-21 12:34:37 +0300

    methods for objects and false

--- multiply.test.js
+++ multiply.test.js
@@ -6,6 +6,15 @@ describe('multiply', () => {
         const expectedResult = 4;
         const actualResult = multiply(2, 2);
         expect(actualResult).toBe(expectedResult);
+        expect({a:1}).toEqual({a:1});
+        expect([1,2,3]).toHaveLength(3);
+        expect([1,2,3]).toContain(3);
+        expect([1,2,3]).not.toContain(10);
+        expect(undefined).toBeUndefined();
+        expect(null).toBeNull();
+        expect(null).toBeFalsy();
+        expect(actualResult).toBeDefined();
+        expect(actualResult).toBeTruthy();
     });
 
     it('should multiply negative numbers', () => {
