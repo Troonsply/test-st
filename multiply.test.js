14aa94a741a705034e29f549f1d7708faa6c74fb
troonsply <troonsply@gmail.com>
2024-05-21 12:16:37 +0300

    describe test

--- multiply.test.js
+++ multiply.test.js
@@ -1,7 +1,20 @@
 import {multiply} from './multiply';
 
+describe('multiply', () => {
+    it('should multiply positive numbers', () => {
+        //AAA - arrange, act, assert
+        const expectedResult = 4;
+        const actualResult = multiply(2, 2);
+        expect(actualResult).toBe(expectedResult);
+    });
 
-test('multiply', () => {
-    const result = multiply(2, 2);
-    expect(result).toBe(4);
+    it('should multiply negative numbers', () => {
+        const result = multiply(-2, -2);
+        expect(result).toBe(4);
+    })
+
+    it('should multiply negative and positive numbers', () => {
+        const result = multiply(-2, 2);
+        expect(result).toBe(-4);
+    })
 })
