acfdcf60cf5948f6000cee62d5ad84a3bdb3116c
troonsply <troonsply@gmail.com>
2024-05-25 09:19:15 +0300

    mocks 1

---
+++ src/__mocks__/basket.mock.js
@@ -0,0 +1,15 @@
+export const basketWithNoQuantity = [
+    {id: 1, title:'Basket No Quantity A', price: 1000, quantity: 2},
+    {id: 2, title:'Basket No Quantity B', price: 2000, quantity: 0},
+    {id: 3, title:'Basket No Quantity C', price: 1200, quantity: 1},
+    {id: 4, title:'Basket No Quantity D', price: 1300, quantity: 0},
+    {id: 5, title:'Basket No Quantity E', price: 1400, quantity: 3},
+    {id: 6, title:'Basket No Quantity F', price: 1500, quantity: 1},
+];
+
+export const filteredBasketWithQuantityOnly = [
+    {id: 1, title:'Basket No Quantity A', price: 1000, quantity: 2},
+    {id: 3, title:'Basket No Quantity C', price: 1200, quantity: 1},
+    {id: 5, title:'Basket No Quantity E', price: 1400, quantity: 3},
+    {id: 6, title:'Basket No Quantity F', price: 1500, quantity: 1},
+]
---
+++ src/__tests__/filterArray.test.js
@@ -0,0 +1,23 @@
+import {filterArray} from '../filterArray';
+import {basketWithNoQuantity, filteredBasketWithQuantityOnly} from "../__mocks__/basket.mock";
+
+const cb = jest.fn();
+describe('filterArray', () => {
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+    it('should not invoke callback when an array is empty', () => {
+        filterArray([], cb);
+        expect(cb).not.toHaveBeenCalled();
+    })
+    it('should invoke provided function as many times as the length of array ', () => {
+        const array = [1, 2, 3];
+        filterArray(array, cb);
+        expect(cb).toHaveBeenCalledTimes(array.length);
+    })
+    it('should filter an array using provided predicate', () => {
+        const hasQuantity = (item) => item.quantity > 0;
+        const filtered = filterArray(basketWithNoQuantity, hasQuantity);
+        expect(filtered).toEqual(filteredBasketWithQuantityOnly);
+    })
+})
