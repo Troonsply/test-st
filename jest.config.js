14e18f7be805687d76ad5fe97edddbc64c7e2c29
troonsply <troonsply@gmail.com>
2024-05-27 16:41:48 +0300

    debug cov reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}','!node_modules/**','!src/**/*.mock.*'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
 };
 
 module.exports = config;
--- src/math.js
+++ src/math.js
@@ -1,9 +1,12 @@
+/* istanbul ignore next */
 export function multiply(a, b) {
     return a * b
 }
+/* istanbul ignore next */
 export function divide(a, b) {
     return a / b
 }
+/* istanbul ignore next */
 export function sum(a, b) {
     return a + b
 }
